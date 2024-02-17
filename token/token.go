530b1ed4bf309a791b75fe35573d6861eac70a23
OPC-16 <chavan.p.omkar@gmail.com>
2024-02-17 15:22:59 +0530

    Convert small subset of Monkey language into tokens

---
+++ token/token.go
@@ -0,0 +1,49 @@
+package token
+
+type TokenType string
+
+type Token struct {
+    Type    TokenType
+    Literal string
+}
+
+var keywords = map[string]TokenType {
+    "fn": FUNCTION,
+    "let": LET,
+}
+
+const (
+    ILLEGAL = "ILLEGAL"
+    EOF = "EOF"
+
+    //Identifiers + literals
+    IDENT = "IDENT"  // add, foobar, x, y, ...
+    INT = "INT"     //3, 234, integers
+
+    // Operators
+    ASSIGN = "="
+    PLUS = "+"
+
+    // Delimiters
+    COMMA = ","
+    SEMICOLON = ";"
+
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACE = "{"
+    RBRACE = "}"
+
+    // Keywords
+    FUNCTION = "FUNCTION"
+    LET = "LET"
+)
+
+// checks the keywords table to see whether the given identifier is in fact a keyword.
+// If it is, it returns the keyword’s TokenType constant.
+// If it isn’t, we just get back token.IDENT, which is the TokenType for all user-defined identifiers.
+func LookupIdent(ident string) TokenType {
+    if tok, ok := keywords[ident]; ok {
+        return tok
+    }
+    return IDENT
+}
