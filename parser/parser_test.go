08ba73a20ddc25df5127f72b26fbfce1295ef502
OPC-16 <chavan.p.omkar@gmail.com>
2024-02-19 00:41:20 +0530

    Now parser can parse Return statements

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -9,9 +9,9 @@ import (
 
 func TestLetStatements(t *testing.T) {
     input := `
-    let x = 5;
-    let y = 10;
-    let foobar = 3234;
+    return 5;
+    return 10;
+    return 10324;
     `
     l := lexer.New(input)
     p := New(l)
@@ -19,25 +19,18 @@ func TestLetStatements(t *testing.T) {
     program := p.ParseProgram()
     checkParserErrors(t, p)
 
-    if program == nil {
-        t.Fatalf("ParseProgram() returned nil")
-    }
     if len(program.Statements) != 3 {
         t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
     }
 
-    tests := []struct {
-        expectedIdentifier string
-    } {
-        {"x"},
-        {"y"},
-        {"foobar"},
-    }
-
-    for i, tt := range tests {
-        stmt := program.Statements[i]
-        if !testLetStatements(t, stmt, tt.expectedIdentifier) {
-            return
+    for _, stmt := range program.Statements {
+        returnStmt, ok := stmt.(*ast.ReturnStatement)
+        if !ok {
+            t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
+            continue
+        }
+        if returnStmt.TokenLiteral() != "return" {
+            t.Errorf("returnStmt.TokenLiteral is not 'return', got=%q", returnStmt.TokenLiteral())
         }
     }
 }
