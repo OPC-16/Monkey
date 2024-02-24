041e0ec038f32345a8ccb48c0fbef438412e9d52
OPC-16 <chavan.p.omkar@gmail.com>
2024-02-24 14:51:34 +0530

    Add helper methods to the parser test suite

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,8 +1,8 @@
 package parser
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 
 	"github.com/OPC-16/Monkey/ast"
 	"github.com/OPC-16/Monkey/lexer"
@@ -60,6 +60,19 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
     }
 }
 
+func checkParserErrors(t *testing.T, p *Parser) {
+    errors := p.Errors()
+    if len(errors) == 0 {
+        return
+    }
+
+    t.Errorf("parser has %d errors", len(errors))
+    for _, msg := range errors {
+        t.Errorf("parser error: %q", msg)
+    }
+    t.FailNow()
+}
+
 func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
     integ, ok := il.(*ast.IntegerLiteral)
     if !ok {
@@ -77,19 +90,6 @@ func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
         return false
     }
     return true
-}
-
-func checkParserErrors(t *testing.T, p *Parser) {
-    errors := p.Errors()
-    if len(errors) == 0 {
-        return
-    }
-
-    t.Errorf("parser has %d errors", len(errors))
-    for _, msg := range errors {
-        t.Errorf("parser error: %q", msg)
-    }
-    t.FailNow()
 }
 
 func testLetStatements(t *testing.T, s ast.Statement, name string) bool {
@@ -114,5 +114,67 @@ func testLetStatements(t *testing.T, s ast.Statement, name string) bool {
         return false
     }
 
+    return true
+}
+
+func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
+    ident, ok := exp.(*ast.Identifier)
+    if !ok {
+        t.Errorf("exp not *ast.Identifier. got=%T", exp)
+        return false
+    }
+
+    if ident.Value != value {
+        t.Errorf("ident.Value not %s. got=%s", value, ident.Value)
+        return false
+    }
+
+    if ident.TokenLiteral() != value {
+        t.Errorf("ident.TokenLiteral not %s. got=%s", value, ident.TokenLiteral())
+        return false
+    }
+
+    return true
+}
+
+func testLiteralExpression(
+    t *testing.T,
+    exp ast.Expression,
+    expected interface{},
+) bool {
+
+    switch v := expected.(type) {
+        case int:
+            return testIntegerLiteral(t, exp, int64(v))
+        case int64:
+            return testIntegerLiteral(t, exp, v)
+        case string:
+            return testIdentifier(t, exp, v)
+    }
+    t.Errorf("type of exp not handled. got=%T", exp)
+    return false
+}
+
+func testInfixExpression(t *testing.T, exp ast.Expression, left interface{},
+    operator string, right interface{}) bool {
+
+    opExp, ok := exp.(*ast.InfixExpression)
+    if !ok {
+        t.Errorf("exp is not ast.InfixExpression. got=%T(%s)", exp, exp)
+        return false
+    }
+
+    if !testLiteralExpression(t, opExp.Left, left) {
+        return false
+    }
+
+    if opExp.Operator != operator {
+        t.Errorf("exp.Operator is not '%s'. got=%q", operator, opExp.Operator)
+        return false
+    }
+
+    if !testLiteralExpression(t, opExp.Right, right) {
+        return false
+    }
     return true
 }
