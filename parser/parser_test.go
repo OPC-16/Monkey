c98e34e9f00f2b565bd1affafe982e89be68f1cc
OPC-16 <chavan.p.omkar@gmail.com>
2024-02-24 15:24:00 +0530

    Add helper method to test boolean literals

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -184,6 +184,8 @@ func testLiteralExpression(
             return testIntegerLiteral(t, exp, v)
         case string:
             return testIdentifier(t, exp, v)
+        case bool:
+            return testBooleanLiteral(t, exp, v)
     }
     t.Errorf("type of exp not handled. got=%T", exp)
     return false
@@ -208,6 +210,25 @@ func testInfixExpression(t *testing.T, exp ast.Expression, left interface{},
     }
 
     if !testLiteralExpression(t, opExp.Right, right) {
+        return false
+    }
+    return true
+}
+
+func testBooleanLiteral(t *testing.T, exp ast.Expression, value bool) bool {
+    bo, ok := exp.(*ast.Boolean)
+    if !ok {
+        t.Errorf("exp not ast.Boolean. got=%T", exp)
+        return false
+    }
+
+    if bo.Value != value {
+        t.Errorf("bo.Value is not %t. got=%t", value, bo.Value)
+        return false
+    }
+
+    if bo.TokenLiteral() != fmt.Sprintf("%t", value) {
+        t.Errorf("bo.TokenLiteral is not %t. got=%s", value, bo.TokenLiteral())
         return false
     }
     return true
