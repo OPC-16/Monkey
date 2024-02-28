34de362c82dbdbde97ec9e296716752fc0f98b34
OPC-16 <chavan.p.omkar@gmail.com>
2024-02-28 19:17:39 +0530

    Evaluate Boolean Literals
    There are only 2 instances of object.Boolean, TRUE and FALSE. Instead of
    returning new object.Boolean for every time we get 'true' or 'false', we
    just return the reference to TRUE or FALSE. Because there is no
    difference between 2 trueS.

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -8,6 +8,21 @@ import (
 	"github.com/OPC-16/Monkey/parser"
 )
 
+func TestEvalBooleanExpression(t *testing.T) {
+    tests := []struct {
+        input    string
+        expected bool
+    } {
+        {"true", true},
+        {"false", false},
+    }
+
+    for _, tt := range tests {
+        evaluated := testEval(tt.input)
+        testBooleanObject(t, evaluated, tt.expected)
+    }
+}
+
 func TestEvalIntegerExpression(t *testing.T) {
     tests := []struct {
         input    string
@@ -21,6 +36,19 @@ func TestEvalIntegerExpression(t *testing.T) {
         evaluated := testEval(tt.input)
         testIntegerObject(t, evaluated, tt.expected)
     }
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+    result, ok := obj.(*object.Boolean)
+    if !ok {
+        t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+        return false
+    }
+    if result.Value != expected {
+        t.Errorf("object has wrong bool value. got=%t, want=%t", result.Value, expected)
+        return false
+    }
+    return true
 }
 
 func testEval(input string) object.Object {
