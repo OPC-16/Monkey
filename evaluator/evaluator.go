f9469d7d20189c3ee658ad18a350bc7d4664e003
OPC-16 <chavan.p.omkar@gmail.com>
2024-03-13 01:00:27 +0530

    Define and call functions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -1,7 +1,7 @@
 package evaluator
 
 import (
-    "fmt"
+	"fmt"
 
 	"github.com/OPC-16/Monkey/ast"
 	"github.com/OPC-16/Monkey/object"
@@ -76,6 +76,18 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
             params := node.Parameters
             body := node.Body
             return &object.Function{Parameters: params, Body: body, Env: env}
+            
+        case *ast.CallExpression:
+            function := Eval(node.Function, env)
+            if isError(function) {
+                return function
+            }
+            args := evalExpression(node.Arguments, env)
+            if len(args) == 1 && isError(args[0]) {
+                return args[0]
+            }
+
+            return applyFunction(function, args)
     }
 
     return nil
@@ -95,6 +107,47 @@ func evalProgram(program *ast.Program, env *object.Environment) object.Object {
         }
     }
     return result
+}
+
+func evalExpression(exps []ast.Expression, env *object.Environment) []object.Object {
+    var result []object.Object
+
+    for _, e := range exps {
+        evaluated := Eval(e, env)
+        if isError(evaluated) {
+            return []object.Object{evaluated}
+        }
+        result = append(result, evaluated)
+    }
+    return result
+}
+
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+    function, ok := fn.(*object.Function)
+    if !ok {
+        return newError("not a function: %s", fn.Type())
+    }
+
+    extendedEnv := extendFunctionEnv(function, args)
+    evaluated := Eval(function.Body, extendedEnv)
+    return unwrapReturnValue(evaluated)
+}
+
+func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
+    env := object.NewEnclosedEnvironment(fn.Env)
+
+    for paramIdx, param := range fn.Parameters {
+        env.Set(param.Value, args[paramIdx])
+    }
+
+    return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+    if returnValue, ok := obj.(*object.ReturnValue); ok {
+        return returnValue.Value
+    }
+    return obj
 }
 
 func evalBlockStatement(block *ast.BlockStatement, env *object.Environment) object.Object {
