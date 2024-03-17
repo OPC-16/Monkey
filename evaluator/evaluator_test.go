cda70556c9ccf6c5d5064d0fe9c52795b51e67e6
OPC-16 <chavan.p.omkar@gmail.com>
2024-03-17 16:03:16 +0530

    Add support for String concatenation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -7,6 +7,20 @@ import (
 	"github.com/OPC-16/Monkey/object"
 	"github.com/OPC-16/Monkey/parser"
 )
+func TestStringConcatenation(t *testing.T) {
+    input := `"Hello" + " " + "World!"`
+
+    evaluated := testEval(input)
+    str, ok := evaluated.(*object.String)
+    if !ok {
+        t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+    }
+
+    if str.Value != "Hello World!" {
+        t.Errorf("String has wrong value. got=%q", str.Value)
+    }
+}
+
 
 func TestStringLiteral(t *testing.T) {
     input := `"Hello world!"`
@@ -120,6 +134,10 @@ func TestErrorHandling(t *testing.T) {
         {
             "foobar",
             "identifier not found: foobar",
+        },
+        {
+            `"Hello" - "World"`,
+            "unknown operator: STRING - STRING",
         },
     }
 
