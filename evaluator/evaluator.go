bc1629ca61dc8035e604750801b0e74c06ff2f7f
OPC-16 <chavan.p.omkar@gmail.com>
2024-03-21 23:00:19 +0530

    Implement 'len' builtin function which takes a string and returns length of it

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -126,14 +126,19 @@ func evalExpression(exps []ast.Expression, env *object.Environment) []object.Obj
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-    function, ok := fn.(*object.Function)
-    if !ok {
-        return newError("not a function: %s", fn.Type())
-    }
+    switch fn := fn.(type) {
+        case *object.Function:
+            extendedEnv := extendFunctionEnv(fn, args)
+            evaluated := Eval(fn.Body, extendedEnv)
+            return unwrapReturnValue(evaluated)
 
-    extendedEnv := extendFunctionEnv(function, args)
-    evaluated := Eval(function.Body, extendedEnv)
-    return unwrapReturnValue(evaluated)
+        case *object.Builtin:
+            return fn.Fn(args...)
+
+        default:
+            return newError("not a function: %s", fn.Type())
+
+    }
 }
 
 func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
@@ -227,12 +232,15 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 }
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
-    val, ok := env.Get(node.Value)
-    if !ok {
-        return newError("identifier not found: " + node.Value)
+    if val, ok := env.Get(node.Value); ok {
+        return val
     }
 
-    return val
+    if builtin, ok := builtins[node.Value]; ok {
+        return builtin
+    }
+
+    return newError("identifier not found: " + node.Value)
 }
 
 func evalStringInfixExpression(operator string, left, right object.Object) object.Object {
